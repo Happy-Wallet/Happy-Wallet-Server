4f5c870b944766ab1444d00de75bba165e180f9e
thiengl2015 <giathien899@gmail.com>
2025-06-27 10:26:27 +0700

    feat(auth): implement forgot password and reset password APIs

---
+++ config/mailConfig.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+module.exports = {
+  EMAIL_USER: process.env.EMAIL_USER,
+  EMAIL_PASS: process.env.EMAIL_PASS,
+};
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 // server.js
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const db = require('./db');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const db = require("./config/db");
+const userRoutes = require("./routes/user");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 // Mount user routes at /users
-app.use('/users', userRoutes); // ✅ now handles GET and POST /users
+app.use("/users", userRoutes);
+
+app.use(express.json());
+app.use("/auth", authRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
