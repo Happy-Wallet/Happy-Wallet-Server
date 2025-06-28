a62ab4bc18ff8a281ea204d3f26117b8a2dcf129
thiengl2015 <giathien899@gmail.com>
2025-06-28 15:14:16 +0700

    Add CRUD API for icons (create, read, update, delete)

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 // server.js
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const userRoutes = require('./routes/user');
-const categoryRoutes = require('./routes/category');
-const transactionRoutes = require('./routes/transaction');
-const savingGoalRoutes = require('./routes/savingGoal');
-const fundRoutes = require('./routes/fund');
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const transactionRoutes = require("./routes/transaction");
+const savingGoalRoutes = require("./routes/savingGoal");
+const fundRoutes = require("./routes/fund");
 const authRoutes = require("./routes/auth");
+const iconRoutes = require("./routes/icon");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 // Mount routes
-app.use('/users', userRoutes);
-app.use('/categories', categoryRoutes);
-app.use('/transactions', transactionRoutes);
-app.use('/saving_goals', savingGoalRoutes);
-app.use('/funds', fundRoutes);
+app.use("/users", userRoutes);
+app.use("/categories", categoryRoutes);
+app.use("/transactions", transactionRoutes);
+app.use("/saving_goals", savingGoalRoutes);
+app.use("/funds", fundRoutes);
 app.use("/auth", authRoutes);
+app.use("/icons", iconRoutes);
+
 app.use(express.json());
 
 const PORT = 3000;
